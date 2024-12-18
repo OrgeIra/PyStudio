30bc669bf5e121113ce51db2fd33a184025f3d69
OrgeIra <dodomatovabdusami@gmail.com>
2024-12-18 19:13:25 +0500

    search

--- goods/urls.py
+++ goods/urls.py
@@ -4,6 +4,7 @@ from .views import product, catalog
 app_name = "goods"
 
 urlpatterns = [
+    path("search/", catalog, name="search"),
     path("<slug:category_slug>/", catalog, name="catalog"),
     path('product/<slug:product_slug>/', product, name="product"),
 ]
